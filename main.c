5c162c3d9412f93587dc093e4da38eeebdb770eb
Edward Lu <edwardzlu98@gmail.com>
2019-02-13 22:57:03 -0800

    Switched regex matching implementation to use dynamic programming, in order to properly match wildcard sequences

--- main.c
+++ main.c
@@ -5,18 +5,24 @@
 #include "../ndn-lite/ndn-error-code.h"
 #include "ndn-trust-schema.h"
 
-static char test_data_pattern_string[] = "<test><test><test>";
-static char test_key_pattern_string[] = "<test><test><test>";
-static char test_data_name_pattern_string_1[] = "/test/test/test";
-static char test_data_name_pattern_string_2[] = "/test/test/fail";
-static char test_key_name_pattern_string_1[] = "/test/test/test";
-static char test_key_name_pattern_string_2[] = "/test/test/fail";
+static const char result_success[] = "SUCCESS";
+static const char result_failure[] = "FAILURE";
 
-static ndn_trust_schema_rule_t test_rule;
+static ndn_trust_schema_rule_t test_rule_1;
+static char test_rule_data_pattern_string_1[] = "<test><test><test>";
+static char test_rule_key_pattern_string_1[] = "<test><test><test>";
+
 static ndn_name_t test_data_name_1;
+static char test_data_name_string_1[] = "/test/test/test";
+
 static ndn_name_t test_data_name_2;
+static char test_data_name_string_2[] = "/test/test/fail";
+
 static ndn_name_t test_key_name_1;
+static char test_key_name_string_1[] = "/test/test/test";
+
 static ndn_name_t test_key_name_2;
+static char test_key_name_string_2[] = "/test/test/fail";
 
 int _initialize_test_objects() {
 
@@ -25,31 +31,31 @@ int _initialize_test_objects() {
   printf("\nThis is a test of a potential schematized trust implementation.\n");
   printf("----------------------------------------------------------------\n");  
 
-  ret_val = ndn_name_from_string(&test_data_name_1, test_data_name_pattern_string_1, sizeof(test_data_name_pattern_string_1));
+  ret_val = ndn_name_from_string(&test_data_name_1, test_data_name_string_1, sizeof(test_data_name_string_1));
   if (ret_val != NDN_SUCCESS) {
     printf("Call to ndn_name_from_string failed, error code: %d\n", ret_val);
     return ret_val;
   }
 
-  ret_val = ndn_name_from_string(&test_data_name_2, test_data_name_pattern_string_2, sizeof(test_data_name_pattern_string_2));
+  ret_val = ndn_name_from_string(&test_data_name_2, test_data_name_string_2, sizeof(test_data_name_string_2));
   if (ret_val != NDN_SUCCESS) {
     printf("Call to ndn_name_from_string failed, error code: %d\n", ret_val);
     return ret_val;
   }
   
-  ret_val = ndn_name_from_string(&test_key_name_1, test_key_name_pattern_string_1, sizeof(test_key_name_pattern_string_1));
+  ret_val = ndn_name_from_string(&test_key_name_1, test_key_name_string_1, sizeof(test_key_name_string_1));
   if (ret_val != NDN_SUCCESS) {
     printf("Call to ndn_name_from_string failed, error code: %d\n", ret_val);
     return ret_val;
   }
-  ret_val = ndn_name_from_string(&test_key_name_2, test_key_name_pattern_string_2, sizeof(test_key_name_pattern_string_2));
+  ret_val = ndn_name_from_string(&test_key_name_2, test_key_name_string_2, sizeof(test_key_name_string_2));
   if (ret_val != NDN_SUCCESS) {
     printf("Call to ndn_name_from_string failed, error code: %d\n", ret_val);
     return ret_val;
   }
-  ret_val = ndn_trust_schema_rule_from_strings(&test_rule,
-					       test_data_pattern_string, sizeof(test_data_pattern_string),
-					       test_key_pattern_string, sizeof(test_key_pattern_string));
+  ret_val = ndn_trust_schema_rule_from_strings(&test_rule_1,
+					       test_rule_data_pattern_string_1, sizeof(test_rule_data_pattern_string_1),
+					       test_rule_key_pattern_string_1, sizeof(test_rule_key_pattern_string_1));
   if (ret_val != NDN_SUCCESS) {
     printf("Call to ndn_trust_schema_rule_from_strings failed, error code: %d\n", ret_val);
     return ret_val;
@@ -62,6 +68,31 @@ int _initialize_test_objects() {
   return 0;
 }
 
+void run_test(ndn_trust_schema_rule_t *test_rule,
+	        const char *test_rule_data_pattern_string, const char*test_rule_key_pattern_string,
+	      ndn_name_t *test_data_name, const char *test_data_name_string,
+	      ndn_name_t *test_key_name, const char *test_key_name_string) {
+  int ret_val = -1;
+  ret_val = ndn_trust_schema_verify_data_name_key_name_pair(test_rule, test_data_name, test_key_name);
+  const char *result;
+  if (ret_val != 0)
+    result = result_failure;
+  else
+    result = result_success;
+
+  printf("Result of call to ndn_trust_schema_verify_key_name for following parameters:\n"
+	 "Rule data pattern: %s\n"
+	 "Rule key pattern: %s\n"
+	 "Data name: %s\n"
+	 "Key name: %s\n"
+	 "Result: %s\n",
+	 test_rule_data_pattern_string, test_rule_key_pattern_string, test_data_name_string, test_key_name_string, result);
+  if (ret_val != 0)
+    printf("Error code: %d\n", ret_val);
+  
+  printf("\n------------------------------------------------------------------------------------------\n\n");
+}
+
 int main() {
 
   int ret_val = -1;
@@ -72,25 +103,15 @@ int main() {
     return -1;
   }
   
-  ret_val = ndn_trust_schema_verify_data_name_key_name_pair(&test_rule, &test_data_name_1, &test_key_name_1);
-  if (ret_val != 0) {
-    printf("Call to ndn_trust_schema_verify_key_name failed for test data name 1, test key name 1, error code: %d\n", ret_val);
-  }
-  else {
-    printf("Call to ndn_trust_schema_verify_key_name succeeded for test data name 1, test key name 1.\n");
-  }
+  run_test(&test_rule_1,
+	   test_rule_data_pattern_string_1, test_rule_key_pattern_string_1,
+	   &test_data_name_1, test_data_name_string_1,
+	   &test_key_name_1, test_key_name_string_1);
 
-  printf("\n------------------------------------------------------------------------------------------\n\n");
-  
-  ret_val = ndn_trust_schema_verify_data_name_key_name_pair(&test_rule, &test_data_name_2, &test_key_name_1);
-  if (ret_val != 0) {
-    printf("Call to ndn_trust_schema_verify_key_name failed for test data name 2, test key name 1, error code: %d\n", ret_val);
-  }
-  else {
-    printf("Call to ndn_trust_schema_verify_key_name succeeded for test data name 1, test key name 1.\n");
-  }
-  
-  printf("\n------------------------------------------------------------------------------------------\n\n");
+  run_test(&test_rule_1,
+	   test_rule_data_pattern_string_1, test_rule_key_pattern_string_1,
+	   &test_data_name_1, test_data_name_string_1,
+	   &test_key_name_2, test_key_name_string_2);
 
   return 0;
 }
--- ndn-trust-schema.c
+++ ndn-trust-schema.c
@@ -5,32 +5,120 @@
 #include "../ndn-lite/ndn-constants.h"
 
 #include <stdbool.h>
+#include <stdio.h>
 
 void _check_name_against_pattern(bool *name_valid, const ndn_trust_schema_pattern_t *pattern, const ndn_name_t* name) {
 
-  int pi = 0, ni = 0;
-  
-  while (pi < pattern->components_size && ni < name->components_size) {    
-    switch (pattern->components[pi].type) {
-    case NDN_TRUST_SCHEMA_SINGLE_NAME_COMPONENT:      
-      if (memcmp(pattern->components[pi].value, name->components[ni].value, pattern->components[pi].size) != 0 ||
-  	  pattern->components[pi].size != name->components[ni].size) {
-  	*name_valid = false;
-      }
-      pi++;
-      ni++;
-      break;
-    case NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT:
-      pi++;
-      ni++;
-      break;
-    case NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT_SEQUENCE:
-      break;
-    }
+  /* printf("Values in pattern: \n"); */
+  /* for (int i = 0; i < pattern->components_size; i++) { */
+  /*   printf("Type of pattern index %d: %d\n", i, pattern->components[i].type); */
+  /*   switch (pattern->components[i].type) { */
+  /*   case NDN_TRUST_SCHEMA_SINGLE_NAME_COMPONENT:   */
+  /*     printf("  Value of pattern index %d: %.*s\n", i, pattern->components[i].size, pattern->components[i].value); */
+  /*   default: */
+  /*     break; */
+  /*   } */
+  /* } */
 
-    if (!(*name_valid))
-      break;
+  /* printf("Values in name: \n"); */
+  /* for (int i = 0; i < name->components_size; i++) { */
+  /*   printf("Value of name index %d: %.*s\n", i, name->components[i].size, name->components[i].value); */
+  /* } */
+
+  /* printf("\n\n"); */
+  
+  /* int pi = 0, ni = 0; */
+  
+  /* while (pi < pattern->components_size && ni < name->components_size) {     */
+  /*   switch (pattern->components[pi].type) { */
+  /*   case NDN_TRUST_SCHEMA_SINGLE_NAME_COMPONENT:       */
+  /*     if (memcmp(pattern->components[pi].value, name->components[ni].value, pattern->components[pi].size) != 0 || */
+  /* 	  pattern->components[pi].size != name->components[ni].size) { */
+  /* 	*name_valid = false; */
+  /*     } */
+  /*     pi++; */
+  /*     ni++; */
+  /*     break; */
+  /*   case NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT: */
+  /*     pi++; */
+  /*     ni++; */
+  /*     break; */
+  /*   case NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT_SEQUENCE: */
+      
+  /*     break; */
+  /*   } */
+
+  /*   if (!(*name_valid)) */
+  /*     break; */
+  /* } */
+
+  int pat_len = pattern->components_size;
+  int name_len = name->components_size;
+  
+  if (pat_len == 0 && name_len == 0) {
+    *name_valid = true;
+    return;
   }
+
+  bool results[name_len+1][pat_len+1];
+
+  // initialize all results to false
+  for (int i = 0; i < name_len+1; i++) {
+    for (int j = 0; j < pat_len+1; j++) {
+      results[i][j] = false;
+    }
+  }
+
+  printf("Value of results array after initialization:\n");
+  for (int i = 0; i < name_len; i++) {
+    for (int j = 0; j < pat_len; j++) {
+      printf("%d ", results[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n\n");
+  
+  // for the base case of comparing an empty string to an empty pattern,
+  // the result is true
+  results[0][0] = true;
+
+  // first check successively larger substrings of the schema pattern containing
+  // the first character of the pattern (i.e. from pattern "abc", check "a", then "ab", then "abc")
+  // against an empty string
+  for (int j = 1; j < pat_len+1; j++) {
+    if (pattern->components[j-1].type != NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT_SEQUENCE)
+      break;
+    results[0][j] = true;
+  }
+
+  for (int i = 1; i < name_len+1; i++) {
+    for (int j = 1; j < pat_len+1; j++) {
+      if (pattern->components[j-1].type == NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT ||
+	  
+	  (pattern->components[j-1].type == NDN_TRUST_SCHEMA_SINGLE_NAME_COMPONENT &&
+	   memcmp(pattern->components[j-1].value, name->components[i-1].value, pattern->components[j-1].size) == 0 &&
+	   pattern->components[j-1].size == name->components[i-1].size)
+	  
+	  )
+      {
+        results[i][j] = results[i-1][j-1];
+      }
+      else if (pattern->components[j-1].type == NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT_SEQUENCE) {
+        results[i][j] = (results[i-1][j] || results[i][j-1]);
+      }
+    }
+  }
+
+  printf("Value of results array after processing:\n");
+  for (int i = 0; i < name_len+1; i++) {
+    for (int j = 0; j < pat_len+1; j++) {
+      printf("%d ", results[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n\n");
+  
+  *name_valid = results[name_len][pat_len];
 }
 
 int
@@ -41,7 +129,12 @@ ndn_trust_schema_verify_data_name_key_name_pair(const ndn_trust_schema_rule_t* r
   bool data_name_valid = true;
   bool key_name_valid = true;
 
+  printf("Checking data name pattern.\n\n");
+  
   _check_name_against_pattern(&data_name_valid, &rule->data_pattern, data_name);
+
+  printf("Checking key name pattern.\n\n");
+  
   _check_name_against_pattern(&key_name_valid, &rule->key_pattern, key_name);
   
   return (data_name_valid && key_name_valid) ? 0 : -1;
